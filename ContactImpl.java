e37fc230116eb53ccecbb742b562f33da4267afb
Jade <jadedickinson@gmail.com>
2016-01-10 11:43:43 +0000

    Added notes on constructors from cw instructions

--- ContactImpl.java
+++ ContactImpl.java
@@ -1,3 +1,13 @@
+/**
+ * The implementation of this interface must have two constructors. The most general
+ * constructor must have three parameters: int, String, String. The first one
+ * corresponds to the ID provided by the ContactManager, the next one corresponds
+ * to the name, and the last one corresponds to the initial set of notes about the
+ * contact. Another, more restricted constructor must have two parameters only:
+ * ID and name. If the ID provided is zero or negative, a IllegalArgumentException
+ * must be thrown. If any of the references / pointers passed as parameters to the
+ * constructor is null, a NullPointerException must be thrown.
+ */
 public class ContactImpl implements Contact {
 
     /**
@@ -30,7 +40,7 @@ public class ContactImpl implements Contact {
      */
     @Override
     public String getNotes() {
-        
+
     }
 
     /**
