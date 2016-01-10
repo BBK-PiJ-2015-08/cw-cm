e412e1153c475f08172513fe6c383f5b0d0d345c
Jade <jadedickinson@gmail.com>
2016-01-10 11:50:25 +0000

    Made template from cw instructions

---
+++ FutureMeetingImpl.java
@@ -0,0 +1,10 @@
+/**
+ * The class implementing this interface must have only one constructor with three
+ * parameters: an ID (int), a date, and a set of contacts that must be non-empty
+ * (otherwise, an IllegalArgumentException must be thrown). If any of the
+ * references/pointers passed as parameters is null, a NullPointerException must be
+ * thrown.
+ */
+public class FutureMeetingImpl implements FutureMeeting {
+
+}
