fe6f990e87e46e3815be137e88521bd6be444991
Jade <jadedickinson@googlemail.com>
2016-02-21 09:07:10 +0000

    CMImplTest-Finished writing plan for order of test creation

--- src/test/ContactManagerImplTest.java
+++ src/test/ContactManagerImplTest.java
@@ -5,11 +5,12 @@ package test;
  * 1. Create a new contact: (AddNewContact)
  * 2. Construct and return sets of contacts: (getContacts(int... ids) and getContacts(String name))
  * 3. Create new meeting in future (addFutureMeeting)
- * 4. Create new meeting in past (
- * 5.
- * 6.
- * 7.
- * 8.
+ * 4. Create new meeting in past (addNewPastMeeting)
+ * 5. Add notes to a meeting (addMeetingNotes)
+ * 6. Get and return individual meetings (getPastMeeting), (getFutureMeeting) & (getMeeting)
+ * 7. Construct and return lists of meetings (getFutureMeetingList), (getMeetingListOn) & (getPastMeetingListFor)
+ * 8. Save all data (flush)
+ * 13 methods to test in total
  */
 
 import java.util.*;
