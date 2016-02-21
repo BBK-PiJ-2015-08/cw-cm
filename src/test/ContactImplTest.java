286b2d7ddf71ba77fb743b1ca6255e8f247a00ee
Jade <jadedickinson@googlemail.com>
2016-02-21 10:18:17 +0000

    ContactImplTest-Fields and setUp/tearDown methods

--- src/test/ContactImplTest.java
+++ src/test/ContactImplTest.java
@@ -12,16 +12,22 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ContactImplTest {
+    private Contact testContact;
+    private int testID;
+    private String testName;
+    private String testNotes;
 
-    
     @Before
     public void setUp() {
-
+        testID = 1001;
+        testName = "Mr Robot";
+        testNotes = "Who knows";
+        testContact = new ContactImpl(testID, testName, testNotes);
     }
 
     @After
     public void tearDown() {
-
+        testContact = null;
     }
 
     //@Test
