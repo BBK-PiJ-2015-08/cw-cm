6a039f3b2bd413f6039a12a9fedb8ac06f6b0540
Jade <jadedickinson@googlemail.com>
2016-02-21 09:02:17 +0000

    CMImplTest-Writing plan for order of test creation

--- src/test/ContactManagerImplTest.java
+++ src/test/ContactManagerImplTest.java
@@ -1,5 +1,17 @@
 package test;
 
+/**
+ * Plan for order of test creation:
+ * 1. Create a new contact: (AddNewContact)
+ * 2. Construct and return sets of contacts: (getContacts(int... ids) and getContacts(String name))
+ * 3. Create new meeting in future (addFutureMeeting)
+ * 4. Create new meeting in past (
+ * 5.
+ * 6.
+ * 7.
+ * 8.
+ */
+
 import java.util.*;
 
 import org.junit.*;
@@ -10,9 +22,13 @@ public class ContactManagerImplTest {
     //fields
 
     @Before
+    public void buildUp() {
 
+    }
     @After
+    public void cleanUp() {
 
+    }
     @Test
 
 }
