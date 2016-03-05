ff48f779d19f5dc878dc93f6875a6f2195fb6bb3
Jade <jadedickinson@googlemail.com>
2016-03-05 16:19:14 +0000

    Wrote setUp of meeting for testing

--- src/test/PastMeetingImplTest.java
+++ src/test/PastMeetingImplTest.java
@@ -3,13 +3,40 @@
  */
 package test;
 
-import code.PastMeeting;
-import code.PastMeetingImpl;
+import code.*;
 import org.junit.Before;
 import org.junit.After;
 import org.junit.Test;
 
+import java.util.*;
+
 import static org.junit.Assert.*;
 
 public class PastMeetingImplTest {
+    private int testMeetingId;
+    private Calendar testDate;
+    private Set<Contact> attended;
+    private Contact contact1;
+    private Contact contact2;
+    private String testNotes;
+    private PastMeeting testMeeting;
+
+    @Before
+    public void setUp() {
+        contact1 = new ContactImpl(3, "Danger Mouse", "Secret agent");
+        contact2 = new ContactImpl(4, "Ernest Penfold", "Kung moggy expert");
+        // January 1st 2016
+        testDate = new GregorianCalendar(2016, 0, 1);
+        testMeetingId = 111;
+        attended = new HashSet<>();
+        attended.add(contact1);
+        attended.add(contact2);
+        testNotes = "Discussing top secret matters";
+        testMeeting = new PastMeetingImpl(testMeetingId, testDate, attended, testNotes);
+    }
+
+    @Test
+    public void testGetNotes() {
+
+    }
 }
