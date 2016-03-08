c92159973afbcb076b5a8c0793142cdcac048f3b
Jade <jadedickinson@googlemail.com>
2016-03-08 12:30:23 +0000

    Revert "Failing from contactImpl - contactId not greater than 0??"
    This reverts commit f75fb12bd63d287bbc103bb672669d92ee24ddfc.

--- src/test/ContactManagerImplTest.java
+++ src/test/ContactManagerImplTest.java
@@ -62,8 +62,8 @@ public class ContactManagerImplTest {
     private Contact testContact1;
     private Contact testContact2;
     private Contact testContact3;
-    private Contact extraContact1;
-    private Contact extraContact2;
+    private Contact additionalContact1;
+    private Contact additionalContact2;
     private Meeting testMeeting;
     private int testMeetingId;
 
@@ -73,18 +73,32 @@ public class ContactManagerImplTest {
         currentDate = Calendar.getInstance();
         allContacts = new HashSet<>();
         allMeetings = new ArrayList<>();
-        contactId = 0;
+<<<<<<< HEAD
 
         testContact1 = new ContactImpl(contactId, "Mowgli", "Man cub");
         testContact2 = new ContactImpl(contactId, "Baloo", "The sleepy brown bear");
         testContact3 = new ContactImpl(contactId, "Louie", "Jungle VIP");
 
-        cm.addNewContact("Mowgli", "Man cub");
-        cm.addNewContact("Baloo", "The sleepy brown bear");
-        cm.addNewContact("Louie", "Jungle VIP");
+        allContacts.add(testContact1);
+        allContacts.add(testContact2);
+        allContacts.add(testContact3);
 
-        //extraContact1 = new ContactImpl(4, "Bagheera", "Found Mowgli");
-        //extraContact2 = new ContactImpl(5, "Kaa", "Not very successful");
+        extraContact1 = new ContactImpl(4, "Bagheera", "Found Mowgli");
+        extraContact2 = new ContactImpl(5, "Kaa", "Not very successful");
+=======
+/**
+        testContact1 = new ContactImpl(contactId, "Mowgli", "Man cub");
+        allContacts.add(testContact1);
+
+        testContact2 = new ContactImpl(contactId, "Baloo", "The sleepy brown bear");
+        allContacts.add(testContact2);
+
+        testContact3 = new ContactImpl(contactId, "Louie", "Jungle VIP");
+        allContacts.add(testContact3);
+
+        additionalContact1 = new ContactImpl(4, "Bagheera", "Found Mowgli");
+        additionalContact2 = new ContactImpl(5, "Kaa", "Not very successful");
+>>>>>>> parent of d0259bc... Minor renaming and reordering
 
         //Group 1 - 1 contact in allContacts
         group1 = new HashSet<>();
