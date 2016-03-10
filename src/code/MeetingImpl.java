2d320adb0b9ee25928f6adc6be39b8574e8bcbd1
Jade <jadedickinson@googlemail.com>
2016-03-10 19:51:53 +0000

    Made MeetingImpl abstract as it should be, though now fails tests

--- src/code/MeetingImpl.java
+++ src/code/MeetingImpl.java
@@ -15,7 +15,7 @@ import java.util.Set;
 /**
  * @author Jade Dickinson jdicki04
  */
-public /**abstract*/ class MeetingImpl implements Meeting {
+public abstract class MeetingImpl implements Meeting, Comparable {
     private int id;
     private Calendar date;
     private Set<Contact> contacts;
