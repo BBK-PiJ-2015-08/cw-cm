cf3fc108bcd1d37cf510c7604abfb3b57103725c
Jade <jadedickinson@googlemail.com>
2016-03-13 12:16:23 +0000

    Tidying comments & removing unused import statement

--- src/code/PastMeetingImpl.java
+++ src/code/PastMeetingImpl.java
@@ -1,14 +1,12 @@
 package code;
 
-//import java.io.Serializable;
 import java.util.Calendar;
 import java.util.Set;
 /**
- * The class implementing this interface must have only one constructor with four
- * parameters: an ID (int), a date, a set of contacts that must be non-empty
- * (otherwise, an IllegalArgumentException must be thrown), and a String containing
- * the notes for the meeting. If any of the references / pointers passed as
- * parameters is null, a NullPointerException must be thrown.
+ * The class implementing this interface must have only one constructor with four parameters: an ID (int), a date, a set
+ * of contacts that must be non-empty (otherwise, an IllegalArgumentException must be thrown), and a String containing
+ * the notes for the meeting. If any of the references / pointers passed as parameters is null, a NullPointerException
+ * must be thrown.
  *
  * @author Jade Dickinson jdicki04
  */
