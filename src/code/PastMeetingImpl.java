4e78fd774c65cf60e1e1f3b00fca774750c4a4e7
Jade <jadedickinson@googlemail.com>
2016-03-14 14:30:02 +0000

    Changes for style reasons

--- src/code/PastMeetingImpl.java
+++ src/code/PastMeetingImpl.java
@@ -3,10 +3,11 @@ package code;
 import java.util.Calendar;
 import java.util.Set;
 /**
- * The class implementing this interface must have only one constructor with four parameters: an ID (int), a date, a set
- * of contacts that must be non-empty (otherwise, an IllegalArgumentException must be thrown), and a String containing
- * the notes for the meeting. If any of the references / pointers passed as parameters is null, a NullPointerException
- * must be thrown.
+ * The class implementing this interface must have only one constructor with
+ * four parameters: an ID (int), a date, a set of contacts that must be
+ * non-empty (otherwise, an IllegalArgumentException must be thrown), and a
+ * String containing the notes for the meeting. If any of the references /
+ * pointers passed as parameters is null, a NullPointerException must be thrown.
  *
  * @author Jade Dickinson jdicki04
  */
@@ -17,7 +18,7 @@ public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
     public PastMeetingImpl(int id, Calendar date, Set<Contact> contacts, String text) {
         super(id, date, contacts);
         if (text == null) {
-            throw new NullPointerException("Please make sure to enter notes that are not null");
+            throw new NullPointerException("Please provide notes that aren't null");
         } else {
             this.text = text;
         }
