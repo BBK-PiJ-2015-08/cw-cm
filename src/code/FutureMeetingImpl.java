640d31597f1789f83618e0bb620b69da54ec1844
Jade <jadedickinson@googlemail.com>
2016-03-14 20:05:50 +0000

    Added serialVersionUID

--- src/code/FutureMeetingImpl.java
+++ src/code/FutureMeetingImpl.java
@@ -7,6 +7,8 @@ import java.util.Set;
  * @author Jade Dickinson jdicki04
  */
 public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting {
+    private static final long serialVersionUID = 2613046855889857597L;
+
     public FutureMeetingImpl(int id, Calendar date, Set<Contact> contacts) {
         super(id, date, contacts);
     }
--- src/code/PastMeetingImpl.java
+++ src/code/PastMeetingImpl.java
@@ -4,11 +4,12 @@ import java.util.Calendar;
 import java.util.Set;
 /**
  * @see PastMeeting
- * 
+ *
  * @author Jade Dickinson jdicki04
  */
 public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
-
+    private static final long serialVersionUID = -4388045743984839327L;
+    
     private String text;
 
     public PastMeetingImpl(int id, Calendar date, Set<Contact> contacts, String text) {
